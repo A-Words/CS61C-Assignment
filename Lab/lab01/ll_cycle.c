02635347bcef93cd8e3d0ec86831f09298277125
A-Words <awordsgg@outlook.com>
2026-03-15 18:11:30 +0800

    finished lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -2,6 +2,14 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    while (hare != NULL && hare->next != NULL) {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
